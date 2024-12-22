728ee2a73e1cd695b69c960696ef337f25d2d4d0
VovaR2D2 <vladimir.ulasevich@email.com>
2024-12-22 13:59:19 -0300

    project Pie: calculation of math Pi value

---
+++ Pi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class PiGrid
+{
+    static void Main()
+    {
+
+        while (true)
+        {
+            double Amount;
+            double.TryParse(Console.ReadLine(), out Amount);
+
+            double pie = 0;
+
+            for (int i = 0; i < Amount; i++)
+            {
+                for (int j = 0; j < Amount; j++)
+                {
+                    if (Math.Sqrt(((j / Amount) * (j / Amount)) + ((i / Amount) * (i / Amount))) <= 1)
+                    {
+                        pie += 1;
+                    }
+                }
+            }
+            Console.WriteLine("Pie: " + pie / (Amount * Amount) * 4);
+            Console.WriteLine("Act: " + "3.14159265359");
+        }
+    }
+}
---
+++ Pi/README.txt
@@ -0,0 +1,1 @@
+Test project to calculate math Pi value programmatically.
