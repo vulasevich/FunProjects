cb5fa59e214e54702b3ab0a969989f7455b58d56
Vladimir <vladimir.ulasevich@gmail.com>
2024-12-28 14:47:53 -0300

    Add files via upload

---
+++ Pi/Program - Copy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class PiRandom
+{
+    static void Main()
+    {
+        while (true)
+        {
+            double Amount;
+            Random random = new Random();
+
+            double.TryParse(Console.ReadLine(), out Amount);
+            double pie = 0;
+
+            for (int i = 0; i < Amount; i++)
+            {
+                double x = (random.NextDouble() * 2) - 1;
+                double y = (random.NextDouble() * 2) - 1;
+                double z = Math.Sqrt(x * x + y * y);
+
+                if (z <= 1)
+                {
+                    pie += 1;
+                }
+            }
+
+            Console.WriteLine("Pi:  " + pie / Amount * 4);
+            Console.WriteLine("Act: " + "3.14159265359");
+        }
+    } 
+}
