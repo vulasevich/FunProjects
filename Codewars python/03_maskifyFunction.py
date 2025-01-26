1d57a5ad0c651b56c355e803fe686b6496192d66
Vladimir <vladimir.ulasevich@gmail.com>
2025-01-26 19:47:29 -0300

    Delete Codewars python directory

--- Codewars python/03_maskifyFunction.py
+++
@@ -1,6 +0,0 @@
-#task is to make a function maskify, which changes all but the last four characters into '#'.
-def maskify(string):
-    if len(string) <= 4:
-        return string
-    
-    return '#' * (len(string) - 4) + string[-4:]
--- Codewars python/05_narcissistic.py
+++
@@ -1,7 +0,0 @@
-def is_narcissistic(number):
-    digits = str(number)
-    num_digits = len(digits)
-    
-    if number == sum(int(digit) ** num_digits for digit in digits):
-        return True
-    return False
